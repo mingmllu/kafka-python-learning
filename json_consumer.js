1a29af1bb1204a1cc1481b49add24cc57d5a0de4
Ming Lu <minglu007@gmail.com>
2018-10-17 22:07:57 -0400

    Add groupdId

--- json_consumer.js
+++ json_consumer.js
@@ -5,8 +5,10 @@ const Kafka = require('no-kafka');
 // use the machine's IP address
 //var broker = '10.0.0.5:9092'
 // If the broker installed on the same host machine, use localhost or 0.0.0.0
-var brokers = '0.0.0.0:9092'
-const consumer = new Kafka.SimpleConsumer ({connectionString: broker})
+var broker = '0.0.0.0:9092'
+// If two consumers are listening on the same topic, they should be assigned different group name
+var group_id = 'view'
+const consumer = new Kafka.SimpleConsumer ({connectionString: broker, groupId: group_id})
 var data = function (messageSet, topic, partition) {
     messageSet.forEach(function (m) {
         console.log(topic, partition, m.offset, m.message.value.toString('utf8'));
